581a5cb73bbaf0cd7fcc625bc40455ffa37d2dd9
Chitra kadyan <chitrakadyan2005@gmail.com>
2026-01-29 00:09:24 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATS7QDzw_rUJQFsZtKTIJs3z0o28XCHaQ",
+  authDomain: "havnlike.firebaseapp.com",
+  projectId: "havnlike",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
